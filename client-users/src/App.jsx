2369fa51bce9ffe3d51de4aaee49a321d51ebd86
unknown <hamzabouhadjila@gmail.com>
2022-04-08 01:05:41 +0200

    install packages for the backend and client-users

---
+++ client-users/src/App.jsx
@@ -0,0 +1,24 @@
+//styles
+import './scss/index.scss';
+import './scss/style.header-spaceship-variant-one.scss';
+import './scss/style.header-spaceship-variant-two.scss';
+import './scss/style.header-spaceship-variant-three.scss';
+import './scss/style.header-classic-variant-one.scss';
+import './scss/style.header-classic-variant-two.scss';
+import './scss/style.header-classic-variant-three.scss';
+import './scss/style.header-classic-variant-four.scss';
+import './scss/style.header-classic-variant-five.scss';
+import './scss/style.mobile-header-variant-one.scss';
+import './scss/style.mobile-header-variant-two.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello from react</h1>
+      
+    </div>
+  );
+}
+
+export default App;
